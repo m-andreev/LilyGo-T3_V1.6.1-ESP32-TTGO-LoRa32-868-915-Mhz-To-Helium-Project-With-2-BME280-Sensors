ec44909bdb929bb12fcab8adbd60d41551a1e20a
Mario Andreev <99554651+m-andreev@users.noreply.github.com>
2023-02-01 13:20:16 +0200

    Update lmic-node-uplink-formatters.js
    added a .js decoder function

--- payload-formatters/lmic-node-uplink-formatters.js
+++ payload-formatters/lmic-node-uplink-formatters.js
@@ -15,7 +15,18 @@ function decodeUplink(input) {
     var warnings = [];
 
     if (input.fPort == 10) {
-        data.counter = (input.bytes[0] << 8) + input.bytes[1];
+        //data.counter = (input.bytes[0] << 8) + input.bytes[1];
+        var received_value = input; //this is the data your RECEIVING PROGAM got
+        var a = received_value >> 16;
+        var b = received_value & 0xffff;
+        
+        var f = a / 10.0;
+        var g = b / 10.0;
+
+        data = {f,g};
+
+        
+        //now the **RECEIVING** SIDE has obtained the original values of `f` and `g`
     }
     else {
         warnings.push("Unsupported fPort");
